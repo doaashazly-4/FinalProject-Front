71899588406c6b3071a50380977364df179f09f0
alfaroukaljwely <aryar4321@gmail.com>
2025-12-22 22:01:06 +0200

    feat: add core application routing including authentication, role guards, and lazy-loaded modules.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,11 @@ export const routes: Routes = [
     loadComponent: () => import('./auth/auth/role-selection/role-selection.component').then(m => m.RoleSelectionComponent)
   },
   {
+    path: 'register',
+    canActivate: [noAuthGuard],
+    loadComponent: () => import('./auth/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
     path: 'login',
     canActivate: [noAuthGuard],
     loadComponent: () =>
