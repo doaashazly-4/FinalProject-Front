3c9aae97f78537b5c9f3b0606cccdd1c8b6a5041
kraken15th <ahmedkarrar79@gmail.com>
2026-01-13 09:49:28 +0200

    fixing UI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -7,6 +10,11 @@ import { authInterceptor } from './shared/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { withInterceptors } from '@angular/common/http';
-import { routes } from './app/app.routes';
-import { authInterceptor } from './app/shared/interceptors/auth.interceptor';
-import { provideServiceWorker } from '@angular/service-worker';
-import { environment } from './environments/environment';
 import { registerLocaleData } from '@angular/common';
 import localeAr from '@angular/common/locales/ar';
 
 registerLocaleData(localeAr);
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideAnimations(), // ✅ THIS LINE FIXES THE ERROR
-    provideHttpClient(
-      withInterceptors([
-        authInterceptor
-      ])
-    ),
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: environment.production,
-    })
-  ]
-});
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
